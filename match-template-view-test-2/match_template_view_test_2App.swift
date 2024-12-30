099b7f32c842b2f66ddca055850e214876cea792
kopyl <oleggggg1997@gmail.com>
2024-12-30 15:37:03 +0200

    Initial Commit

---
+++ match-template-view-test-2/match_template_view_test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  match_template_view_test_2App.swift
+//  match-template-view-test-2
+//
+//  Created by Oleh Kopyl on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct match_template_view_test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
