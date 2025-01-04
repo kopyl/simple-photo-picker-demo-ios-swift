e8ef8cb2a41e7028d3e1fe30388c4ad826c6d011
kopyl <oleggggg1997@gmail.com>
2025-01-04 14:35:50 +0200

    Slightly fixed crop handles cropping

--- match-template-view-test-2/ContentView.swift
+++ match-template-view-test-2/ContentView.swift
@@ -329,6 +329,7 @@ struct ImageScrollView: View {
                         }
                     }
                 }
+                .scrollClipDisabled(true)
                 .frame(width: geometry.size.width, height: geometry.size.height)
                 .scrollTargetLayout()
                 .scrollTargetBehavior(.viewAligned)
